2f44c72b04b630f2305d357e084637aff20a5cb5
richard <richard.turra@sensedata.com.br>
2020-12-07 15:20:54 -0300

    whell canvas scrolling

--- src/index.js
+++ src/index.js
@@ -1,50 +1,96 @@
-import React, { createContext, useRef, useContext, Fragment } from "react";
+import React, {
+  createContext,
+  useRef,
+  useContext,
+  Fragment,
+  Children,
+  cloneElement
+} from "react";
 import { useFrame, useThree } from "react-three-fiber";
 import lerp from "lerp";
 
 const offsetContext = createContext(0);
-let top = 0;
 
+// variable to control the top displacement
+export let top = {
+  value: 0,
+  get top() {
+    return value;
+  },
+  set top(val) {
+    value = val;
+  }
+};
+
+// function wrapper to include the scrollbar and the whell scroll function
 function Scanvas({ children }) {
   const sections_array = children.props.children.filter(
     item => item.type.name === "Section"
   );
 
-  const domContent = useRef();
-  const scrollArea = useRef();
-
-  const onScroll = e => {
-    top = e.target.scrollTop;
+  const whellScrol = e => {
+    let newTop = top.value + e.deltaY;
+    if (newTop >= 0 && newTop <= window.innerHeight) {
+      newTop = top.value + e.deltaY;
+      top.value = newTop;
+      document.getElementById("scrollid").scrollBy(0, e.deltaY);
+    }
   };
+
+  const barScroll = e => {
+    top.value = e.target.scrollTop;
+  };
+
+  let canvasClone = Children.map(children, item =>
+    cloneElement(item, { onWheel: whellScrol })
+  );
+
   return (
     <Fragment>
-      {children}
-      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
-        <div style={{ position: "sticky", top: 0 }} ref={domContent}></div>
-        <div style={{ height: `${sections_array.length * 100}vh` }}></div>
+      {canvasClone}
+      <div
+        className="scrollArea"
+        id="scrollid"
+        onScroll={barScroll}
+        style={{
+          position: "absolute",
+          top: "0px",
+          right: "0px",
+          height: "100vh",
+          overflow: "scroll"
+        }}
+      >
+        <div
+          id="scrollbar"
+          style={{
+            height: `${sections_array.length * 100}vh`
+          }}
+        ></div>
       </div>
     </Fragment>
   );
 }
 
-function Section({ children, offset, ...props }) {
+// section wrapper that control the section position
+function Section({ children, offset, factor = 1, ...props }) {
   const { offset: parentOffset, sectionHeight, aspect } = useSection();
   const ref = useRef();
   offset = offset !== undefined ? offset : parentOffset;
   useFrame(() => {
     const curY = ref.current.position.y;
-    const curTop = top / aspect;
-    ref.current.position.y = lerp(curY, curTop, 0.1);
+    const curTop = top.value / aspect;
+    ref.current.position.y = lerp(curY, curTop * factor, 0.1);
   });
   return (
     <offsetContext.Provider value={offset}>
-      <group {...props} position={[0, -sectionHeight * offset, 0]}>
+      <group {...props} position={[0, -sectionHeight * offset * factor, 0]}>
         <group ref={ref}>{children}</group>
       </group>
     </offsetContext.Provider>
   );
 }
 
+// function that returns important parameters
 function useSection() {
   const { size, viewport } = useThree();
   const offset = useContext(offsetContext);
@@ -57,6 +103,7 @@ function useSection() {
   const contentMaxWidth = canvasWidth * (mobile ? 0.8 : 0.6);
   const sectionHeight = canvasHeight;
   const aspect = size.height / viewportHeight;
+
   return {
     aspect,
     viewport,
